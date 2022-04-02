9ec00bfe43d6866e14b90242243d0e702d076092
mahoromon <geyimin55@gmail.com>
2022-04-02 08:33:12 +0800

    test

--- robot305_gazebo/scripts/walker_demo.py
+++ robot305_gazebo/scripts/walker_demo.py
@@ -6,12 +6,12 @@ from robot305_gazebo.robot305 import Robot305
 
 if __name__=="__main__":
     rospy.init_node("walker_demo")
-    
+
     rospy.loginfo("Instantiating Robot305 Client")
     robot305=Robot305()
     rospy.sleep(1)
- 
-    rospy.loginfo("Robot305 Walker Demo Starting")
+
+    rospy.loginfo("Robot305 CPG-style Walker Demo Starting")
 
 
     robot305.set_walk_velocity(0.2,0,0)
@@ -27,5 +27,5 @@ if __name__=="__main__":
     robot305.set_walk_velocity(1,1,0)
     rospy.sleep(5)
     robot305.set_walk_velocity(0,0,0)
-    
+
     rospy.loginfo("Robot305 Walker Demo Finished")
